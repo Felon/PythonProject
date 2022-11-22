47a776b408e06b9eee20df21c1e9f23322853f5b
Alex <aleksey.tng@gmail.com>
2022-11-22 22:28:09 +0700

    added solution for testing the promo pages and looking for a bug

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,14 @@ class LoginPageLocators:
     REGISTRATION_EMAIL = (By.NAME, 'registration-email')
     REGISTRATION_PSW = (By.NAME, 'registration-password1')
     REGISTRATION_PSW_CONFIRM = (By.NAME, 'registration-password2')
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CLASS_NAME, 'btn-add-to-basket')
+    BUTTON_VIEW_BASKET = (By.CSS_SELECTOR, '.btn-group > .btn')
+    BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
+    ALERT_MESSAGE = (By.CSS_SELECTOR, "div.alert:nth-child(1) strong")
+    ALERT_BOOK_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    ADDED_BOOK_NAME = (By.CSS_SELECTOR, '.col-sm-4 a')
+    ADDED_BOOK_PRICE = (By.CSS_SELECTOR, '.col-sm-1 p')
